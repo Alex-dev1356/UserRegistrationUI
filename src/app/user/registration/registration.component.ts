a82f93de486a4d41a4953857b2a9a8fc2de4fd96
Alex-dev1356 <asoliven26@yahoo.com>
2026-05-09 12:44:37 +0800

    Creating a form using Reactive Foorms Module and Form Builder.We also create a form group object

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styles: ``
 })
 export class RegistrationComponent {
+// Injecting FormBuilder to create a reactive form for user registration
+formBuilder = inject(FormBuilder);
+
+form = this.formBuilder.group({
+  fullName: [''],
+  email: [''],
+  password: [''],
+  confirmPassword:['']
+});
 
 }
