36b92bfddd8bb5e4a69bf16f3e6e3389a0135221
Alex-dev1356 <asoliven26@yahoo.com>
2026-05-24 15:36:55 +0800

    Dealing with the validations inside the form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -15,10 +15,24 @@ export class LoginComponent {
 
   formBuilder = inject(FormBuilder);
 
+  isSubmitted: boolean = false;
+
   //Creating a group of form controls for the login form using the FormBuilder service. This allows us to easily manage and validate the form inputs for user login, such as email and password fields.
   form = this.formBuilder.group({
     email: ['', [Validators.required]],
     password: ['', [Validators.required]]
   })
+
+  hasDisplayableError(controlName: string) : Boolean {
+    const control = this.form.get(controlName); 
+    return Boolean(control?.invalid) && 
+    (Boolean(control?.touched) || this.isSubmitted || Boolean(control?.dirty));
+  }
+
+  onSubmit() {
+    this.isSubmitted = true; 
+
+    console.log(this.form.value);
+  }
 
 }
