c1f1cfea7f4b09dfe360351efcc08bdcc51966ef
Alex-dev1356 <asoliven26@yahoo.com>
2026-06-15 15:03:53 +0800

    Applying Fading Animation for Signin and Signup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
   provideHttpClient(),//Invokes the provideHttpClient function to set up the necessary providers for making HTTP requests in the application. This allows components and services to inject HttpClient and use it to communicate with backend APIs or perform other HTTP operations. 
+  provideAnimationsAsync(),//Invokes the provideAnimationsAsync function to set up the necessary providers for using animations in the application. This allows components to use Angular's animation features, such as triggering animations on certain events or states, and ensures that the necessary dependencies for animations are properly configured in the application.
   provideToastr() //Invokes the provideToastr function to set up the necessary providers for using the ngx-toastr library in the application. This allows components and services to inject ToastrService and use it to display toast notifications for various events such as success messages, error messages, etc.
 ]
 };
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { RegistrationComponent } from './registration/registration.component';
-import { RouterOutlet } from '@angular/router';
+import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
+import { trigger, style, animate, transition, query } from '@angular/animations';
 
 @Component({
   selector: 'app-user',
@@ -8,8 +9,26 @@ import { RouterOutlet } from '@angular/router';
   imports: [RegistrationComponent, 
     RouterOutlet],
   templateUrl: './user.component.html',
-  styles: ``
+  styles: ``,
+  animations: [
+    //defining the animation
+    trigger('routerFadeIn', [
+      transition('* <=> *', [
+        query(':enter', [
+          style({ opacity: 0 }),
+          animate('1s ease-in-out', style({ opacity: 1 }))
+        ],
+        { optional: true })
+      ])
+    ])
+  ]
 })
 export class UserComponent {
 
+  private context = inject(ChildrenOutletContexts);
+
+  // This method returns the URL of the current route being displayed in the router outlet. It uses the ChildrenOutletContexts service to access the context of the primary outlet and retrieves the URL from the route information. This URL can be used to trigger animations based on route changes, allowing for dynamic transitions between different views or components in the application when navigating through routes. By using this method in conjunction with Angular's animation system, you can create smooth and visually appealing transitions between different pages or sections of your application as users navigate through it.
+  getRouteUrl() {
+    return this.context.getContext('primary')?.route?.url;
+  }
 }
